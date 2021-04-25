b3b84c55c7c8accffa822f5f0eb47d2d0cee2f5e
Yipu <yipu.wang.utexas@gmail.com>
2021-04-25 14:54:40 +0800

    add images and embedded video for L-Tree

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// A Helper function to create an element and attach it to another node;
+let createElement = (element, parent)=>{ 
+    let node = document.createElement(element);
+    parent.appendChild(node);
+    return node
+};
+
+window.onload = function(){
+    let gallery = this.document.getElementById("ltree-gallery");
+    for(let i = 1; i <=12; i++)
+    {
+        let name = `LTree_${i}.png`
+        let str = './assets/ltree/' + name;
+        let img = createElement("img", gallery);
+        img.setAttribute("class", "portfolio-image");
+        img.setAttribute("src", str);
+        img.setAttribute("alt", name);
+        this.console.log(img);
+
+    }
+}
